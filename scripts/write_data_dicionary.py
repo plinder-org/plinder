55d40ffa40dc529cd99b727524979375d666b200
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-07-17 16:56:00 -0400

    chore: remove publish scripts

---
+++ scripts/write_data_dicionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import pandas as pd
+
+df = pd.concat([
+    pd.read_csv(path, sep='\t') for path in
+    Path('src/plinder-data/column_descriptions').rglob('*.tsv')
+]).reset_index(drop=True)
+with open('src/plinder-data/data_dictionary.md', 'w') as f:
+    f.write(df.to_markdown() + '\n')
