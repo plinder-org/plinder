0bf03837d3c431f79490f1d61951f9ff1444b8e0
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-07-19 17:00:52 -0400

    chore: clean up some inits

--- src/plinder/data/__init__.py
+++ src/plinder/data/__init__.py
@@ -1,10 +1,2 @@
 # Copyright (c) 2024, Plinder Development Team
 # Distributed under the terms of the Apache License 2.0
-"""Namespace package root for plinder-data."""
-from pathlib import Path
-
-from plinder.data._version import _get_version
-
-__version__ = _get_version()
-
-_root = Path(__file__).parent
--- src/plinder/eval/__init__.py
+++ src/plinder/eval/__init__.py
@@ -1,2 +1,3 @@
 # Copyright (c) 2024, Plinder Development Team
 # Distributed under the terms of the Apache License 2.0
+
