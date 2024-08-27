53042afb52da6bbf49f7687134613171d4983e94
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-08-27 09:02:27 -0400

    style: ruff

--- src/plinder/core/split/plot.py
+++ src/plinder/core/split/plot.py
@@ -46,7 +46,9 @@ def label_has_mms_within_subset(
         f"mmp series after subset {len(mms_df.index)} records for {mms_df['system_id'].nunique()} systems"
     )
     # count number of unique ligands (not counting repeats)
-    mms_count = mms_df.groupby("congeneric_id")["congeneric_ligand_ccd_code"].agg('nunique')
+    mms_count = mms_df.groupby("congeneric_id")["congeneric_ligand_ccd_code"].agg(
+        "nunique"
+    )
     mms_df["mms_unique_count"] = mms_df["congeneric_id"].map(mms_count)
     good_mms_systems = set(
         mms_df[mms_df["mms_unique_count"] >= minimum_count]["system_id"]
