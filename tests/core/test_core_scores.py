7b429e19a86da7ba3f858e764a28e46318942a00
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-09-11 14:48:55 -0400

    test: add a test for doc base

--- tests/core/test_core_scores.py
+++ tests/core/test_core_scores.py
@@ -93,7 +93,9 @@ def test_query_links(read_plinder_mount):
 
 def test_query_links_columns(read_plinder_mount):
     system_id = "4dd7__1__1.A__1.B"
-    df = scores.query_links(columns=["reference_system_id"], filters=[("query_system", "==", system_id)])
+    df = scores.query_links(
+        columns=["reference_system_id"], filters=[("query_system", "==", system_id)]
+    )
     assert len(df.index)
     assert "reference_system_id" in df.columns
     assert "kind" in df.columns
--- tests/data/test_plinder_data.py
+++ tests/data/test_plinder_data.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2024, Plinder Development Team
 # Distributed under the terms of the Apache License 2.0
 def test_ost():
-    try:
-        import openstructure as ost
-    except (ImportError, ModuleNotFoundError):
-        import ost
+    import ost
 
     assert ost is not None
