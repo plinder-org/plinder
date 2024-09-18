9a855c6511236c2bd95689abd23b90347a0167ec
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-09-18 13:10:28 -0400

    docs: whitelist ligand_rdkit_validation away from warning

--- docs/tablegen.py
+++ docs/tablegen.py
@@ -75,7 +75,8 @@ def generate_table(description_dir: Path, output_html_path: Path) -> None:
         # Columns are considered mandatory if they contain a value in all rows
         is_mandatory[i] = is_value.all()
         # Get the first non-empty value as an example
-        if not is_value.any():
+        # whitelist ligand_rdkit_validation because we're only sampling the first 1000 rows
+        if not is_value.any() and column_name not in ["ligand_rdkit_validation"]:
             logger.warning(f"Column '{column_name}' has no values.")
             examples[i] = repr(None)
         else:
