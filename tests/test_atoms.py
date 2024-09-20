e22def97a0a52fb93586b6b2e3f7778c53ee87fd
Thomas Duignan <thomas.j.duignan@gmail.com>
2024-09-19 20:38:05 -0400

    style: ruff

--- tests/test_atoms.py
+++ tests/test_atoms.py
@@ -1,11 +1,8 @@
 # Copyright (c) 2024, Plinder Development Team
 # Distributed under the terms of the Apache License 2.0
 from biotite.structure import AtomArray
-from rdkit import Chem
-
 from plinder.core.structure.atoms import (
     atom_array_from_cif_file,
-    atom_array_to_rdkit_mol,
 )
 
 
