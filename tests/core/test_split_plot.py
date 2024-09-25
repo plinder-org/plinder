21b19eef9a5570d443511e256437e299d8fb1313
Ninjani <janani.durairaj@gmail.com>
2024-09-25 22:17:08 +0200

    test: make plot use old split file

--- tests/core/test_split_plot.py
+++ tests/core/test_split_plot.py
@@ -1,4 +1,4 @@
-def test_split_plot(write_plinder_mount, tmp_path):
+def test_split_plot(write_plinder_mount, split_plot_split_file, tmp_path):
     from plinder.core.split.plot import SplitPropertiesPlotter
 
     output_dir = tmp_path / "split_plots"
@@ -6,7 +6,7 @@ def test_split_plot(write_plinder_mount, tmp_path):
 
     plotter = SplitPropertiesPlotter.from_files(
         data_dir=write_plinder_mount,
-        split_file=write_plinder_mount / "splits" / "split.parquet",
+        split_file=split_plot_split_file,
         output_dir=output_dir,
         # stratified_train_test_file=write_plinder_mount / "strat" / "train_vs_test_data" / "test_set.parquet",
         # stratified_train_val_file=write_plinder_mount / "strat" / "train_vs_val_data" / "val_set.parquet",
