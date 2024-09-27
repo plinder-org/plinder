d261dd06be6e72479c1e43151fa2e671a4e30892
yusuf1759 <mr.adeshina.yusuf@gmail.com>
2024-09-27 21:19:19 +0000

    fix: linting

--- tests/core/test_transforms.py
+++ tests/core/test_transforms.py
@@ -21,7 +21,9 @@ def test_transform_abc(read_plinder_mount):
         ("19hc__1__1.A_1.B__1.V_1.X_1.Y", ["foo"]),
     ],
 )
-def test_select_atom_types_structure_transform(read_plinder_mount, system_id, atom_types):
+def test_select_atom_types_structure_transform(
+    read_plinder_mount, system_id, atom_types
+):
     valid_atom_names = set(pc.ALL_ATOMS)
     expected_atom_names = set(atom_types).intersection(valid_atom_names)
     s = PlinderSystem(system_id=system_id).holo_structure
