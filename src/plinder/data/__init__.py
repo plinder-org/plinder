a943f7f968e8c44854adb3a407cc1d994f26f7a9
OleinikovasV <v.oleinikovas@gmail.com>
2025-02-17 11:48:37 +0100

    chore: pin networkit==11.0 due to numpy version clash

--- src/plinder/data/__init__.py
+++ src/plinder/data/__init__.py
@@ -9,7 +9,7 @@ except (ImportError, ModuleNotFoundError):
     raise ImportError(
         dedent(
             """\
-            plinder.data requires the OpenStructureToolkit >= 2.8.0 (ost) and networkit >= 11.0 to be installed.
+            plinder.data requires the OpenStructureToolkit >= 2.8.0 (ost) and networkit == 11.0 to be installed.
             Please refer to the documentation for installation instructions and current limitations.
             See details here:
 
