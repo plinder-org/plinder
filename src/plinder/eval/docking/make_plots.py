62899b0d7ef6a6d38d31f770f4818b418f5745f5
OleinikovasV <v.oleinikovas@gmail.com>
2025-02-17 10:53:11 +0100

    bugfix: one aggregate score per model

--- src/plinder/eval/docking/make_plots.py
+++ src/plinder/eval/docking/make_plots.py
@@ -43,8 +43,11 @@ class EvaluationResults:
     def from_scores_and_data_files(
         cls, score_file: Path, data_file: Path, output_dir: Path, top_n: int = 10
     ) -> "EvaluationResults":
-        # use only one score per system when plotting aggregated scores
-        scores_df = pd.read_parquet(score_file).drop_duplicates("reference")
+        # for eval we use aggregate (wave) scores
+        # multi ligand systems have them duplicated per each ligand entry
+        # drop them to avoid multiple counting when socring the same pose
+        # i.e. keep one aggregate score (wave) per model
+        scores_df = pd.read_parquet(score_file).drop_duplicates(["reference", "rank"])
         data_df = pd.read_parquet(data_file)
         merged_df = scores_df[scores_df["reference"].isin(data_df["system_id"])].merge(
             data_df, left_on="reference", right_on="system_id", how="left"
